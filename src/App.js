0fac33f15a06cdf95d0acc160dc01c9272ac8d19
alexey-kiselev333 <Kiselev.Alexey.Sergeevich1@yandex.ru>
2021-08-27 17:44:25 +0300

    Homework is ready

--- src/App.js
+++ src/App.js
@@ -16,12 +16,11 @@ function App() {
     const [items, setItems] = React.useState([])
     const [cartItems, setCartItems] = React.useState([])
 
-    const [paginationSize, setPaginationSize] = React.useState("10");
+    const [paginationSize, setPaginationSize] = React.useState("8");
     const [currentPage, setCurrentPage] = React.useState(1);
 
     const [searchValue, setSearchValue] = React.useState("")
     const debouncedSearchTerm = useDebounce(searchValue, 1000);
-
 
 
     const paginationHandler = (e) => {
@@ -43,8 +42,8 @@ function App() {
     const handleChange = (e) => {
         const inputName = e.target.name;
         let inputValue = e.target.value;
-        if (e.target.type==="date"){
-            inputValue=e.target.value.split('-')[1]+"/"+e.target.value.split('-')[0]
+        if (e.target.type === "date") {
+            inputValue = e.target.value.split('-')[1] + "/" + e.target.value.split('-')[0]
         }
 
         setQueryParams({...queryParams, [inputName]: inputValue});
@@ -63,18 +62,17 @@ function App() {
     }, [queryParams])
 
 
-
     React.useEffect(() => {
         axios(`https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=${paginationSize}${searchValue}`).then(res => setItems(res.data))
     }, [debouncedSearchTerm])
 
     React.useEffect(() => {
         axios(`https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=${paginationSize}${searchValue}`).then(res => setItems(res.data))
-    }, [currentPage,paginationSize,debouncedSearchTerm])
+    }, [currentPage, paginationSize, debouncedSearchTerm])
 
 
     const sendRequest = () => {
-            axios(`https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=${paginationSize}${filteres}${searchValue}`).then(res => setItems(res.data))
+        axios(`https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=${paginationSize}${filteres}${searchValue}`).then(res => setItems(res.data))
     }
 
     return (
@@ -86,14 +84,15 @@ function App() {
             <div className="content distance-pad">
                 <div className="d-flex align-center mb-40 justify-between ">
                     <h1>Лучшее пиво</h1>
-                <SearchBlock setSearchValue={setSearchValue}/>
+                    <SearchBlock setSearchValue={setSearchValue}/>
                 </div>
                 <div className="justify-between">
                     <ManyFilters handleChange={handleChange} sendRequest={sendRequest}/>
                 </div>
                 <div className="d-flex flex-wrap">
                     {items.map((item) => (
-                        <Card title={item.name} price={item.price} image_url={item.image_url}
+                        <Card title={item.name} abv={item.abv} ebc={item.ebc}  price={item.price} image_url={item.image_url}
+                              ph={item.ph} description={item.description}
                               onFavorite={() => console.log('Добавили в закладки')}
                               onPlus={(obj) => onAddToCart(obj)}/>
                     ))}
@@ -101,7 +100,7 @@ function App() {
                 <Pagination
                     paginationHandler={paginationHandler}
                     currentPageHandler={currentPageHandler}
-                    lastPage={items.length<paginationSize}
+                    lastPage={items.length < paginationSize}
                     currentPage={currentPage}
                 />
             </div>
